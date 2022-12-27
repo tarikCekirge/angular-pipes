ea2acb1cb8eff441709d16f4f3c7d9e393e7ed86
Tarik Cekirge <tcekirge@gmail.com>
2022-12-27 18:49:25 +0300

    angular-pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-pipes';
+  name: any = '';
+  date: any = '';
+  amount: number = 0;
+  height: number = 0;
+  mile: number = 0
+  lessons = {
+    name: 'Math',
+    subject: 'Numbers',
+    currentPoint: '80'
+  }
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
+  onHeightChange(value: string) {
+    this.height = parseFloat(value);
+  }
+
+  onMilesChange(value: number) {
+    this.mile = value
+  }
 }
---
+++ src/app/pipes/convert.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'convert'
+})
+export class ConvertPipe implements PipeTransform {
+
+  transform(value: any, type: string): any {
+    switch (type) {
+      case 'km':
+        return value * 1.609344;
+      case 'm':
+        return value * 1.609344 * 1000;
+      case 'cm':
+        return value * 1.609344 * 1000 * 1000;
+      default:
+        throw new Error("Not Found")
+    }
+  }
+
+}
